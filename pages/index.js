4c77316d1d4ec9748d618c4c17dd0fd821d416ff
Prashant Thevar <prashant.thevar@slick-digital.co.uk>
2023-03-19 14:24:08 +0530

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+import { RecipeCard } from "../components/RecipeCard";
+export default function Recipes({ recipes }) {
   return (
     <div className="recipe-list">
-      Recipe List
+      {recipes.map((recipe, key) => (
+        <div key={key}>
+          <RecipeCard recipe={recipe} />
+        </div>
+      ))}
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
     </div>
-  )
+  );
+}
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export async function getStaticProps() {
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
 }
